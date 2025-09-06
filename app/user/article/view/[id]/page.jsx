2540f1a2698ae9b50524ab404f9d73d4d74a4b5f
Ali Dhiaus Syamsi <155028767+AliDhiaus@users.noreply.github.com>
2025-09-06 15:45:43 +0700

    Update page.jsx

--- app/user/article/view/[id]/page.jsx
+++ app/user/article/view/[id]/page.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 const page = () => {
   return (
     <div className='shadow p-5 rounded-md'>
-      <h1 className="text-3xl text-center font-bold mb-6">View Article</h1>
       <ViewArticle />
     </div>
   )
